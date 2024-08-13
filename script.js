8f1342c9bf7913e39ea2f56d1c8b5dfe3733f25f
lina marin <linamarin2011@gmail.com>
2024-08-13 08:52:01 -0500

    ejemplos de java script con variable var

---
+++ script.js
@@ -0,0 +1,33 @@
+/* Datos de salida */
+
+
+
+/* Datos de entrada */
+/* prompt("Ingrese su nombre");/* es como el scaner en java */
+
+
+/* Variables Java Script 
+*Hay 3 tipos de variables (var, let, const)
+*las variables no tienen tipo de dato
+* las 3 variables almacenan cualquier tipo de dato 
+*/
+
+
+/* ejemplo 1 
+ var nombreCompleto = "lina marin";
+console.log(nombreCompleto); */
+
+
+
+
+/* ejemplo2 */
+/* Datos de proceso */
+var numeroUno = parseInt(prompt("Ingrese un número: ")); 
+var numeroDos = parseInt(prompt("Ingrese un número: "));
+console.log(numeroUno + numeroDos);
+
+/* Todo dato ingresado por teclado por defecto es un string */
+
+
+/* Parseo de datos (conversion de datos) */
+
