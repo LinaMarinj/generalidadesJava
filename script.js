92c2bf91b82c10f5b441f8bda2ac67422582e4df
lina marin <linamarin2011@gmail.com>
2024-08-13 10:05:50 -0500

    eje,plo de diferencias entre las variables en js

--- script.js
+++ script.js
@@ -22,12 +22,62 @@ console.log(nombreCompleto); */
 
 /* ejemplo2 */
 /* Datos de proceso */
-var numeroUno = parseInt(prompt("Ingrese un número: ")); 
+/* var numeroUno = parseInt(prompt("Ingrese un número: ")); 
 var numeroDos = parseInt(prompt("Ingrese un número: "));
-console.log(numeroUno + numeroDos);
+console.log(numeroUno + numeroDos); */
+
+
+
+
+
+
+
+
+
+
 
 /* Todo dato ingresado por teclado por defecto es un string */
 
 
 /* Parseo de datos (conversion de datos) */
+
+/* VArible var 
+Las variables de tipo var permiten sobreescritura de la variable copmo tal y su contenido
+esto no debe suceder por ningun motivo pero js no lo pintara como error, js lo aceptara y trabajara con la ultima asignacion del contenido  */
+
+/* VArible let 
+Las variables de tipo let no permiten sobreescritura de la variable del mismo nombre pero si de contenido  */
+
+
+
+/* VArible const 
+Las variables de tipo const no permiten la sobre escritura ni de la variable ni del contenido  */
+
+/* las variables tipo lef y const son las mas utilizadas hoy en día */
+
+
+
+/* -para datos de salida son 
+-para datos de entrada  son  prompt("Ingrese su nombre"); es como el scaner en java 
+
+
+
+-datos de proceso son todas las operaciones  matematicas: +,-,*,/...
+
+y operaciones logicas:
+
+logicos(generan un resultado booleano: true o false):
+<    --> menor que(excluyente)
+>    --> mayor que (excluyente)
+<=   --> menor o igual que(incluyente)
+>=   --> mayor o igual que(incluyente)
+==   --> comparacion simple compara que sea igual 
+===  --> comparacion estricta  compara que sean iguales y compara el tipo de dato
+!=   --> negacion simple
+!==  --> negacion estricta 
+
+*/
+
+
+console.log(3<5)
 
